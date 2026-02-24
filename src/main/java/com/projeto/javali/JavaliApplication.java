e63d0c7b5d1dfc16cc1d5374cd8d7c43356200e3
Luiz <sadandudu123@gmail.com>
2026-02-24 19:43:53 -0300

    Aula24/02/2026

---
+++ src/main/java/com/projeto/javali/JavaliApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.javali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/javali/JavaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.javali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
