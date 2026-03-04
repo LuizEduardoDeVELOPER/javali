72b9853ee0d04e378c4ec5d5b53052407250451c
Luiz <sadandudu123@gmail.com>
2026-03-03 21:02:17 -0300

    Projetofinal03/03/2026

--- src/main/java/com/projeto/javali/HelloController.java
+++ src/main/java/com/projeto/javali/HelloController.java
@@ -1,13 +1,30 @@
 package com.projeto.javali;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+import org.springframework.ui.Model; // Serve para levar dados para o HTML
+@Controller
 public class HelloController {
 
-    @GetMapping("/teste")
-    public String index() {
-        return "O sistema Javali está online e respondendo!";
+    @Autowired
+    private TendaRepository repository; // Isso conecta o mensageiro ao cérebro
+
+   @GetMapping("/")
+public String index(Model model) {
+    // repository.findAll() vai no MySQL e pega tudo o que tem lá
+    model.addAttribute("listaDeTendas", repository.findAll());
+    return "index";
+}
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Tenda novaTenda) {
+        // Pega os dados do formulário e salva no banco de dados real
+        repository.save(novaTenda);
+        
+        // Depois de salvar, recarrega a página para você ver o formulário limpo
+        return "redirect:/";
     }
 }
